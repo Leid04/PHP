2887298ca458e384aac503541f46c470bbcfc9dc
Denys <denys.msi04@gmail.com>
2023-12-12 14:12:40 +0100

    libro v2

--- 2DAW/2_EV/POO/03-Libro.php
+++ 2DAW/2_EV/POO/03-Libro.php
@@ -7,26 +7,31 @@
 
         Añade dos constantes a la clase `Libro`: `IDIOMA` con el valor 'Español' y `NUMERO_PAGINAS` con un valor fijo. 
         Modifica el método `mostrarDetalles()` para incluir también estos datos.*/
-    class Libro{
-        public $titulo;
-        public $autor;
-        public $anioPubli;
-        const IDIOMA = "Español";
-        const NUMERO_PAGINAS = 233;
-
-        function __construct($titulo, $autor, $anioPubli){
-            $this->titulo = $titulo;
-            $this->autor = $autor;
-            $this->anioPubli = $anioPubli;
+        class Libro{
+            public $titulo;
+            public $autor;
+            public $anioPubli;
+            const IDIOMA = "Español";
+            const NUMERO_PAGINAS = 233;
+        
+            function __construct($titulo, $autor, $anioPubli){
+                $this->titulo = $titulo;
+                $this->autor = $autor;
+                $this->anioPubli = $anioPubli;
+            }
+        
+            function mostrarDetalles(){
+                echo "Título: $this->titulo<br>";
+                echo "Autor: $this->autor<br>";
+                echo "Año de Publicación: $this->anioPubli <br>";
+                echo "Idioma: " . self::IDIOMA . "<br>";
+                echo "Número de páginas: " . self::NUMERO_PAGINAS . "<br>";
+            }
         }
-        function mostrarDetalles(){
-            echo "Título:$this->titulo<br>";
-            echo "Autor:$this->autor<br>";
-            echo "Año de Publicación: $this->anioPubli <br>";
-            echo "Idioma es: $this->titulo::IDIOMA";
-            echo "Numero de paginas es: $this->titulo::NUMERO_PAGINAS";
-        }
-    }
-    $libro1 = new Libro("El Señor de los Anillos", "J.R. Tolkien", 2003);
-    $libro1 = new Libro("C++ de 0 a PRO", "Alex Cil", 2004);
+        
+        $libro1 = new Libro("El Señor de los Anillos", "J.R. Tolkien", 2003);
+        $libro2 = new Libro("C++ de 0 a PRO", "Alex Cil", 2004);
+        
+        $libro1->mostrarDetalles();
+        $libro2->mostrarDetalles();
 ?>
