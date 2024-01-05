1a3ba154cac0d91fce283a668fee9a909845e733
Denys Revutskyi <denys.msi04@gmail.com>
2024-01-05 14:40:11 +0100

    galeria codigo +-

--- MySQL/Galeria/admin/index.php
+++ MySQL/Galeria/admin/index.php
@@ -1,27 +1,75 @@
 <!DOCTYPE html>
-<html lang="es">
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>Formulario con Bootstrap</title>
+<html lang="en">
 
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-  </head>
-  <body>
-    <div class="container mt-5">
-        <div class="row justify-content-center">
-          <?php
-            $page = $_GET['page'];
-            switch($page){
-              case 'login': 
-                include "./login.php";
-                break;
-              case 'new': 
-                include "./registro.php";
-                break;
-            }
-          ?>
-        </div>
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Thumbnail Gallery - Start Bootstrap Template</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="/assets/bootstrap/css/thumbnail-gallery.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="/assets/css/estilos.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+      <?php
+
+        $page = $_GET['page'];
+
+        switch ( $page) {
+          case 'login':
+            include "includes/login.inc.php";
+            break;
+          
+          case 'new':
+            include "includes/new.inc.php";
+            break;
+        }
+        
+
+      ?>
+    
+
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p></p>
+                </div>
+            </div>
+        </footer>
+
     </div>
-  </body>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="/assets/bootstrap/js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="/assets/bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+
 </html>
--- MySQL/Galeria/index.php
+++ MySQL/Galeria/index.php
@@ -1,40 +1,129 @@
-<!DOCTYPE html>
-<html lang="es">
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>Galería con Bootstrap y MySQL</title>
+<?php
 
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-  </head>
-  <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  include "common/utils.php";
+  include "common/config.php";
+  include "common/mysql.php";
+
+  /*include "includes/config.php";
+
+  debug ( $config);*/
+
+
+  # conectamos con la base de datos
+  $connection = Connect( $config['database']);
+
+  $sql  = "select * from images where enabled = 1 order by id desc";
+
+  $rows = ExecuteQuery( $sql, $connection);
+
+  Close( $connection);
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Thumbnail Gallery - Start Bootstrap Template</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="assets/bootstrap/css/thumbnail-gallery.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
-            <a class="navbar-brand" href="#">Galería</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="admin/index.php?page=login">Admin</a>
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">GALLERY</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    
+                    <li>
+                        <a href="admin/index.php?page=login">[admin]</a>
                     </li>
                 </ul>
             </div>
+            <!-- /.navbar-collapse -->
         </div>
+        <!-- /.container -->
     </nav>
-    <div class="container mt-4">
-        <div class="row">
-            <div class="col-md-4 mb-4">
-                <img src="imagen1.jpg" class="img-fluid" alt="Imagen 1">
-            </div>
-            <div class="col-md-4 mb-4">
-                <img src="imagen2.jpg" class="img-fluid" alt="Imagen 2">
-            </div>
-            <div class="col-md-4 mb-4">
-                <img src="imagen3.jpg" class="img-fluid" alt="Imagen 3">
-            </div>
+
+    <!-- Page Content -->
+    
+      <!-- Page Content -->
+    <div class="container">
+
+      <div class="row">
+
+        <div class="col-lg-12">
+            <h1 class="page-header">Galleria</h1>
         </div>
+
+        <?php
+
+          foreach ( $rows as $row) 
+          {
+            echo '<div class="col-lg-3 col-md-4 col-xs-6 thumb">
+                  <a class="thumbnail" href="#">
+                      <img class="img-responsive css_img" src="images/'.$row['file'].'" alt="">
+                      
+                  </a>'.$row['name'].'
+              </div>';
+          }
+
+        ?>
+
+      </div>
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p></p>
+                </div>
+            </div>
+        </footer>
+
     </div>
-  </body>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="assets/bootstrap/js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+
+</body>
+
 </html>
