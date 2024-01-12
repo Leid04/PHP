9db006ff32d82ca93611d329c31b8506badd420a
Denys <denys.msi04@gmail.com>
2024-01-12 13:21:23 +0100

    mejorar animal

--- 2DAW/2_EV/BBDD/listado_animales.php
+++ 2DAW/2_EV/BBDD/listado_animales.php
@@ -17,6 +17,7 @@
           <th>Nombre</th>
           <th>Especie</th>
           <th>Edad</th>
+          <th>Acciones</th>
         </tr>
         <?php
           while ($fila = $resultado->fetch_assoc()) {
@@ -25,9 +26,42 @@
               echo "<td>{$fila['nombre']}</td>";
               echo "<td>{$fila['especie']}</td>";
               echo "<td>{$fila['edad']}</td>";
+              echo "<td>";
+                echo "<button onclick='borrarAnimal({$fila['id']})'>Borrar</button>";
+              echo "</td>";
             echo "</tr>";
           }
         ?>
     </table>
+    <script>
+      const borrarAnimal = (animalId) => {
+        const options = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ id: animalId }),
+        }
+        const getData = (data) => {
+          if (data.success) {
+            alert('Animal borrado exitosamente.');
+            location.reload();
+          } else {
+            alert('Error al borrar el animal.');
+          }
+        }
+        const getError = (er) => {
+          console.error('Error:', error);
+          alert('Error al procesar la solicitud.');          
+        }
+
+        if (confirm("Enserio quieres borrar este animal?")) {
+          fetch('borrar_animal.php', options)
+          .then(response => response.json())
+          .then(data => getData())
+          .catch(error => getError());
+        }
+      }
+    </script>
   </body>
 </html>
