9d0d47f18884925471052f2ce4c239ccdad5bc42
Denys <denys.msi04@gmail.com>
2024-02-02 14:01:40 +0100

    ferreteria completo pdo

--- 2DAW/2_EV/BBDD-PDO/ferreteria.php
+++ 2DAW/2_EV/BBDD-PDO/ferreteria.php
@@ -1,24 +1,44 @@
 <?php
-class conectaBD{ 
-  private $conn = null;
-  function __construct($database="ferreteria"){ 
-    $dsn ="mysql:host=localhost;dbname=$database";
-    try {$this->conn = new PDO($dsn,'denys','');} 
-    catch (PDOException $e){die( "Error: {$e->getMessage()} <br/>");}
+class ConectaBD {
+  private $conn;
+
+  function __construct($database="ferreteria") {
+    $dsn = "mysql:host=localhost;dbname=$database";
+    try {
+      $this->conn = new PDO($dsn, 'denys', '');
+    } catch (PDOException $e) {
+      die("Error: {$e->getMessage()} <br/>");
+    }
   }
-  function insertaFila($name,$address,$cp){
-    $datos = [':par1'=>$name, ':par2'=>$address, ':par3'=>$cp];
-    $sql   = 'INSERT INTO ferreteria (name, address, cp)
-              VALUES (:par1, :par2, :par3)';
-    $res   = $this->conn->prepare($sql);
+
+  function listarTiendas() {
+    $filas = [];
+    $sql = 'SELECT name, address FROM ferreterias';
+    $res = $this->conn->query($sql);
+    if ($res) {
+      $res->setFetchMode(PDO::FETCH_ASSOC);
+      while ($r = $res->fetch()) {
+        $filas[] = $r;
+      }
+    }
+    return $filas;
+  }
+
+  function borrarTienda($id) {
+    $datos = [':par1' => $id];
+    $sql = 'DELETE FROM ferreterias WHERE id = :par1';
+    $res = $this->conn->prepare($sql);
     return $res->execute($datos);
   }
-  function borraFerreteria($id){
-    $datos = [':par1'=>$id];
-    $sql   = 'DELETE FROM empleados WHERE id = :par1' ;
-    $res   = $this->conn->prepare($sql);
+
+  function crearTienda($name, $address, $cp) {
+    $datos = [':par1' => $name, ':par2' => $address, ':par3' => $cp];
+    $sql = 'INSERT INTO ferreterias (name, address, cp) VALUES (:par1, :par2, :par3)';
+    $res = $this->conn->prepare($sql);
     return $res->execute($datos);
   }
-  function __destruct(){ $this->conn = null; }
+  function __destruct() { $this->conn = null; }
 }
-$ferreteria = new conectaBD("ferreteria");
+$ferreteria = new ConectaBD("ferreteria");
+$tiendas = $ferreteria->listarTiendas();
+print_r($tiendas);
