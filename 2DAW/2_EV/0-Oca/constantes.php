2d69dea48b2e0593d1d2406db52822108d26041a
Denys Linux <denys.msi04@gmail.com>
2024-03-06 19:35:37 +0100

    ocav2

---
+++ 2DAW/2_EV/0-Oca/constantes.php
@@ -0,0 +1,5 @@
+<?php
+  const MIN_JUGADORES = 2;
+  const MAX_JUGADORES = 4;
+  const COLORES = ["#FF0000", "#00FF00", "#0000FF", "#FFFF00"];
+?>
---
+++ 2DAW/2_EV/0-Oca/seleccion_jugadores.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+  <meta charset="UTF-8">
+  <meta name="author" content="Denys Revutskyi">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Selección de jugadores</title>
+  <link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+    <div id="main">
+      <form action="funcion.php" method="POST">
+        <?php
+          if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (!empty($_POST["jugadores"])) {
+              $jugadores = (int)$_POST["jugadores"];//Recoger numero de jugadores.
+              echo "Seleccionaste $jugadores jugadores.<br><br>";
+            } else { echo "No se seleccionó ningún número de jugadores."; }
+          } else { echo "Error: No se recibió una solicitud POST."; }
+
+          echo "<input type='hidden' name='jugadores' value='$jugadores'>";//Enviar numero de jugadores.
+
+          require("constantes.php");
+          for ($i = 1; $i <= $jugadores; $i++) {
+            echo "<label for='jugador[$i]'>Jugador $i:</label>";
+            echo "<input type='text' name='jugador[$i]'><br>";
+            
+            echo "<input type='color' list='colores[$i]' name='bgColor[$i]'>";
+            
+              echo "<datalist id='colores[$i]'>";
+                foreach (COLORES as $value) {
+                  echo "<option value='$value'>$value</option>";
+                }
+              echo "</datalist><br><br>";
+          }
+        ?>
+        <button type="submit" id="centrado">A JUGAR!!!</button>
+      </form>
+    </div>
+  </body>
+</html>
